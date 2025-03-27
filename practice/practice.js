115417e4ae0d9cdbd5e5738dcef3ba5feb065162
manoj singh <manojsingh00963@gmail.com>
2025-03-27 14:05:46 +0530

    change somthing

--- practice/practice.js
+++ practice/practice.js
@@ -1,1 +1,9 @@
 console.log('hello world!');
+
+// Function to add two numbers
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// Example usage
+console.log(addNumbers(5, 3)); // Output: 8
